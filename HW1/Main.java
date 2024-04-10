2143ecf9e6cecc176e6ba08b954d9e5b87c6afed
MinhQuan231205 <23020416@vnu.edu.vn>
2024-04-10 22:01:26 +0700

    Create Main.java

---
+++ HW1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Đàm Lê Minh Quân \t 23020416 \t AI2 \t MinhQuan231205 \t 23020416@vnu.edu.vn");
+        for (int i=9; i>=1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
