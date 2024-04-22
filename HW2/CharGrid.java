827bdacf5eb5b0bc7beacf426397dee4edf6b8ea
MinhQuan231205 <23020416@vnu.edu.vn>
2024-04-22 07:14:12 +0700

    Create CharGrid.java

---
+++ HW2/CharGrid.java
@@ -0,0 +1,77 @@
+public class CharGrid {
+    private int n, m;
+    private char[][] grid;
+    void getCharGrid(char[][] grid, int n, int m) {
+        this.grid = grid;
+        this.n = n;
+        this.m = m;
+    }
+    int charArea(char ch) {
+        int s=0, fou=0;
+        int up=n, down=1, left=m, right=1;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                if (grid[i][j] == ch) {
+                    fou = 1;
+                    if (i < up) up = i;
+                    if (i > down) down = i;
+                    if (j < left) left = j;
+                    if (j > right) right = j;
+                }
+
+            }
+        }
+        if (fou == 0)
+            s = 0;
+        else if (fou == 1)
+            s = (right-left+1) * (down-up+1);
+        return s;
+    }
+
+    int countPlus() {
+        int countplus = 0;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                int bra = 1, cnt = 0, check = 0;
+                char ck = grid[i][j];
+
+                while(true) {
+                    if (j+bra < m+1) {
+                        if (grid[i][j+bra] == ck) {
+                            cnt++;
+                        }
+                    }
+                    if (j-bra >= 1) {
+                        if (grid[i][j-bra] == ck) {
+                            cnt++;
+                        }
+                    }
+                    if (i+bra < n+1) {
+                        if (grid[i+bra][j] == ck) {
+                            cnt++;
+                        }
+                    }
+                    if (i-bra >= 1) {
+                        if (grid[i-bra][j] == ck) {
+                            cnt++;
+                        }
+                    }
+
+                    if (cnt == 4) {
+                        cnt=0;
+                        check = 1;
+                        bra++;
+                    }
+                    else if (cnt == 0 && check == 1) {
+                        countplus++;
+                        break;
+                    }
+                    else if (cnt >= 0 && cnt <= 3) {
+                        break;
+                    }
+                }
+            }
+        }
+        return countplus;
+    }
+}
